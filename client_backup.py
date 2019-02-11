19cb73136f6cd512d004b1f280da272c2ad6f85b
harald25 <harald@dira.no>
2019-02-11 17:04:25 +0100

    Add functions for creating and deleting LVM snapshots

--- client_backup.py
+++ client_backup.py
@@ -1,2 +1,21 @@
+#! /usr/bin/env python3.6
+
+import subprocess
+
+def main():
+    createLvmSnapshot("/dev/centos/root")
+    deleteLvmSnapshot("/dev/centos/snap")
+
+# Need to add a check of the path to see that it is valid
+def createLvmSnapshot(lvm_path):
+    subprocess.run(['lvcreate', '-L512MB', '-s', '-n' 'snap', lvm_path])
+    subprocess.run(['mount','/dev/centos/snap','/mnt/snap'])
+
+# Need to add a check of the path to see that it is valid
+def deleteLvmSnapshot(snapshot_path):
+    subprocess.run(['umount', '/mnt/snap'])
+    subprocess.run(['lvremove', '-y', snapshot_path])
+
+
 if __name__ == "__main__":
     main()
