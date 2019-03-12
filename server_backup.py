50fc5908e5641a54dc6286bccb957baedcf5e4e7
Harald <harald@dira.no>
2019-03-12 13:22:58 +0100

    upådate

--- server_backup.py
+++ server_backup.py
@@ -4,15 +4,15 @@ from datetime import datetime
 import subprocess
 
 def main():
-    createClone("backup/backup-ipsec/","5_inc","incremental")
+    createClone("backup/backup-ipsec","5_inc","incremental")
 
 
 def createClone(main_dataset_name, sub_dataset_name, backup_type):
     time_now = datetime.today().strftime('%Y-%m-%d--%H-%M-%S')
     #Take snapshot
-    subprocess.run(['zfs', 'snapshot', main_dataset_name +'@' + time_now])
+    subprocess.run(['zfs', 'snapshot', main_dataset_name +'/@' + time_now])
     #Make clone
-    subprocess.run(['zfs', 'clone', main_dataset_name +'@' + time_now, main_dataset_name + '_' + backup_type + '_' + time_now ])
+    subprocess.run(['zfs', 'clone', main_dataset_name +'/@' + time_now, main_dataset_name + '_' + backup_type + '_' + time_now ])
 
 def readSchedule():
     print("Schedule")
