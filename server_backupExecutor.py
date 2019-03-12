f8e1c55178e6efbdd3c3eed524f7a157db12c2a6
Harald <harald@dira.no>
2019-03-12 13:46:42 +0100

    Split program to multiple files

--- server_backupExecutor.py
+++ server_backupExecutor.py
@@ -2,8 +2,10 @@
 
 from datetime import datetime
 import subprocess
+import os.path
 
 def main():
+
     createClone("backup/backup-ipsec","5_inc","incremental")
 
 
@@ -13,15 +15,6 @@ def createClone(main_dataset_name, sub_dataset_name, backup_type):
     subprocess.run(['zfs', 'snapshot', main_dataset_name + '/' + sub_dataset_name +'@' + time_now])
     #Make clone
     subprocess.run(['zfs', 'clone', main_dataset_name + '/' + sub_dataset_name +'@' + time_now, main_dataset_name + '/' + backup_type + '_' + time_now ])
-
-def readSchedule():
-    print("Schedule")
-
-def checkLockFile1():
-    print("Lock 1")
-
-def checkLockFile2():
-    print("Lock 2")
 
 if __name__ == "__main__":
     main()
---
+++ server_backupInitiator.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3.6
+
+from datetime import datetime
+import subprocess
+
+def main():
+    readSchedule()
+
+def readSchedule():
+    print("Schedule")
+
+def checkLockFile1():
+    print("Lock 1")
+
+
+if __name__ == "__main__":
+    main()
