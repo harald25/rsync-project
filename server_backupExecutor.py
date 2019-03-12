1a7e047a0c4163999d59d994f692bd6d7e724e25
Harald <harald@dira.no>
2019-03-12 15:55:00 +0100

    Fix syntax

--- server_backupExecutor.py
+++ server_backupExecutor.py
@@ -7,8 +7,12 @@ from paramiko import SSHClient
 def main():
 
     #createClone("backup/backup-ipsec","5_inc","incremental")
-    initiateClient("backup-ipsec", "root")
-    sys.exit(0)
+    (stdout, stderr, exit_code) = initiateClient("backup-ipsec", "root")
+    if stdout:
+        print(stdout)
+    if stderr:
+        print (stderr)
+    sys.exit(exit_code)
 
 
 def createClone(main_dataset_name, sub_dataset_name, backup_type):
@@ -23,9 +27,12 @@ def initiateClient(client, username):
     ssh.load_system_host_keys()
     ssh.connect(client, username = username)
     (ssh_stdin, ssh_stdout, ssh_stderr) = ssh.exec_command('/root/zfsync_test/rsync-project/client_backup.py')
-    print(ssh_stdout.readlines())
-    print(ssh_stdout.channel.recv_exit_status())
+    stdout = ssh_stdout.readlines()
+    stderr = ssh_stderr.readlines()
+    exit_code = ssh_stdout.channel.recv_exit_status()
     ssh.close()
+    return (stdout, stderr, exit_code)
+
 
 
 if __name__ == "__main__":
