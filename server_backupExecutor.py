d59d9a5e2f8df73d50d976a67da37dca558343b2
Harald <harald@dira.no>
2019-03-12 14:49:23 +0100

    Update client info

--- server_backupExecutor.py
+++ server_backupExecutor.py
@@ -8,7 +8,7 @@ from paramiko import SSHClient
 def main():
 
     #createClone("backup/backup-ipsec","5_inc","incremental")
-    initiateClient("")
+    initiateClient("backup-ipsec", "root")
 
 
 def createClone(main_dataset_name, sub_dataset_name, backup_type):
@@ -20,8 +20,9 @@ def createClone(main_dataset_name, sub_dataset_name, backup_type):
 
 def initiateClient(client, username):
     ssh = SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
     ssh.load_system_host_keys()
-    ssh.connect(username + '@' + client + '/root/backuptest')
+    ssh.connect(username + '@' + client + '/root/zfsync_test/rsync-project')
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('./client_backup.py')
     print(ssh_stdout)
 
