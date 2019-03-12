646a6a37020fb14c90e96122c8b2d518a7a14224
Harald <harald@dira.no>
2019-03-12 10:38:15 +0100

    Add config files

---
+++ server_backup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3.6
+
+def main():
+    print("Hello World!")
+
+
+def createClone():
+    print("Clone")
+
+def readSchedule():
+    print("Schedule")
+
+def checkLockFile1():
+    print("Lock 1")
+
+def checkLockFile2():
+    print("Lock 2")
+
+if __name__ == "__main__":
+    main()
