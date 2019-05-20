68280f8a72ac4b42ecf28a4ba6ad6f2e891fa3eb
Harald Simon Dahl <haralddahl@Haralds-MacBook-Pro.local>
2019-05-20 12:51:05 +0200

    Work on create clone function

--- server_backupExecutor.py
+++ server_backupExecutor.py
@@ -3,6 +3,7 @@
 import subprocess, os.path, paramiko, sys
 from datetime import datetime
 from paramiko import SSHClient
+(EXIT_OK, EXIT_WARNING, EXIT_CRITICAL, EXIT_UNKNOWN) = (0,1,2,3)
 
 def main():
 
@@ -27,11 +28,40 @@ def main():
 
 
 def createClone(dataset_name, backup_type):
-    time_now = datetime.today().strftime('%Y-%m-%d--%H-%M-%S')
-    #Take snapshot
-    subprocess.run(['zfs', 'snapshot', dataset_name +'@' + time_now])
-    #Make clone
-    subprocess.run(['zfs', 'clone', dataset_name +'@' + time_now, dataset_name + '/' + backup_type + '_' + time_now ])
+    time_now = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    dataset_list = subprocess.run(['zfs', 'list', '-t', 'filesystem', '-o', 'name', '-H', '-r', dataset_name],encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if dataset_list.stderr:
+        if "dataset does not exist" in dataset_list.stderr:
+            print("The specified dataset does not exist and must be created before running the backup job")
+            sys.exit(EXIT_CRITICAL)
+        else:
+            print("Unknown error while running zfs list to list current data sets")
+            sys.exit(EXIT_CRITICAL)
+
+    else:
+
+        if backup_type == "full":
+            print ("Backup type: full")
+            new_dataset = subprocess.run(['zfs', 'create', dataset_name +'/' + time_now + "_full"],encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if new_dataset.stderr:
+                print (new_dataset.stderr)
+                sys.exit(EXIT_CRITICAL)
+            else:
+                return new_dataset.stdout
+
+        if backup_type == "diff":
+            print "Backup type: differential"
+
+        if backup_type == "inc":
+            print "Backup type: incremental"
+            #Take snapshot
+            subprocess.run(['zfs', 'snapshot', dataset_name +'@' + time_now],encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            #Make clone
+            subprocess.run(['zfs', 'clone', dataset_name +'@' + time_now, dataset_name + '/' + backup_type + '_' + time_now ],encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+
 
 def initiateClient(client, username):
     ssh = SSHClient()
