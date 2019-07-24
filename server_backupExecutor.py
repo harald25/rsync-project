6f4ffd08cce3d0da5f64e3f39eaceecdef1cb669
Harald Simon Dahl <haralddahl@Haralds-MBP.localdomain>
2019-07-24 18:00:37 +0200

    Fix typo

--- server_backupExecutor.py
+++ server_backupExecutor.py
@@ -118,7 +118,7 @@ def rsync_files(client, volume, lv_suffix, dataset):
             return 1 # 1 = error
         else:
             print("New directory created successfully: "+backup_dest_dir)
-            write_to_log("critical","Unable to create directory: "+backup_dest_dir,backupjob_log_file)
+            write_to_log("New directory created successfully: "+backup_dest_dir,backupjob_log_file)
             rsync_process = subprocess.run(['rsync', '-az', '--delete', '-e', 'ssh', 'root@'+client+':'+lv_mount_path, backup_dest_dir],
                                             encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             if rsync_process.stderr:
@@ -174,7 +174,7 @@ def create_dataset(root_dataset_name, backup_type):
         if not dataset_list:
             #If dataset_list is empty there are no previous backups, and we will force a full backup
             print("No previous backups. Backup type force to 'full'")
-            write_to_log("info","No previous backups. Backup type force to 'full'", backupjob_log_file)
+            write_to_log("info","No previous backups. backup_type forced to 'full'", backupjob_log_file)
             backup_type = "full"
 
         if backup_type == "full":
